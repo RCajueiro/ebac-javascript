f567349f51a88ff2a32da3e10c15653de8a0cc71
Ramon <ramoncajueiro@me.com>
2024-07-25 04:51:06 -0300

    Atividade javascript

---
+++ array.js
@@ -0,0 +1,24 @@
+function indices(array) {
+  
+    let maior = 0
+    let menor = 0
+  
+    for (let i = 1; i < array.length; i++) {
+      if (array[i] > array[maior]) {
+        maior = i
+      }
+      if (array[i] < array[menor]) {
+        menor = i
+      }
+    }
+  
+    return {
+      maior,
+      menor
+    }
+  }
+
+  const a = [23, 87, 45, 19, 74, 92, 10, 33, 67, 50]
+  const { maior, menor } = indices(a)
+  console.log(`O índice do maior valor é ${maior}, e o índice do menor valor é ${menor}`)
+  
---
+++ mdc.js
@@ -0,0 +1,14 @@
+function mdc(valor, resto) {
+    while (resto!== 0) {
+      let temp = resto
+      resto = valor % resto
+      valor = temp
+    }
+    return valor
+  }
+
+  const a = 516
+  const b = 36
+  const MDC = mdc(a, b)
+  console.log(`O MDC de ${a} e ${b} é ${MDC}.`)
+  
